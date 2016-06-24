179a2248650d1293f0ca2e60a95341e7bcaf8998
android777 <18249507381@163.com>
2016-06-24 16:04:19 +0800

    add log

--- app/src/main/java/zc/com/example/testgit/MainActivity.java
+++ app/src/main/java/zc/com/example/testgit/MainActivity.java
@@ -5,10 +5,11 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-
+	public static String tag="zc";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		Log.i(tag,"hello");
     }
 }
